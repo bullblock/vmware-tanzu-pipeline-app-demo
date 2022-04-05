5c1d3cbdd64fa46dd04a6c7fef1cbd1ce92aa8e7
devops <devops@concourse.wdc.lab>
2022-04-05 08:28:14 +0800

    updated

--- app-demo.sh
+++ app-demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 kubectl apply -f psp.yaml 
 kubectl create ns sock-shop
-kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='Qh#!3#ocuYDlq0th' --docker-email="steven.liang@icloud.com" -n sock-shop
+kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='' --docker-email="steven.liang@icloud.com" -n sock-shop
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"docker-hub\"}]}" -n sock-shop
-kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='Qh#!3#ocuYDlq0th' --docker-email="steven.liang@icloud.com"
+kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='' --docker-email="steven.liang@icloud.com"
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"docker-hub\"}]}"
 
