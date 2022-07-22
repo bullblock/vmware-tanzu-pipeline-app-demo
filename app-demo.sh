da4b71696ba62e150064c3fa23cb963a96dfbc86
bullblock <steven.liang@icloud.com>
2022-07-22 10:43:08 +0800

    updated

--- app-demo.sh
+++ app-demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kubectl apply -f psp.yaml 
 kubectl create ns sock-shop
-kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='' --docker-email="steven.liang@icloud.com" -n sock-shop
+kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='Qh#!3#ocuYDlq0th' --docker-email="steven.liang@icloud.com" -n sock-shop
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"docker-hub\"}]}" -n sock-shop
 kubectl create secret docker-registry docker-hub --docker-username=powellcrack --docker-password='' --docker-email="steven.liang@icloud.com"
 kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"docker-hub\"}]}"
